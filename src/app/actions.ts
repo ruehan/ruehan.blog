9eed31305dce3aedbaf7d765e80c15d54e3754b0
GYU HAN <ruehan98@gmail.com>
2024-07-16 23:46:00 +0900

    Feat : 태그 별 포스트 가져오기 API 구현

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,5 +1,6 @@
 "use server";
 
+import prisma from "@/lib/prisma";
 import fetch from "node-fetch";
 
 const apiKey = process.env.CLOUDFLARE_API_KEY;
@@ -34,4 +35,57 @@ export default async function getDetails(videoId: string) {
 			message: "failed",
 		};
 	}
+}
+
+export async function getPost() {
+	const post = await prisma.post.findMany({
+		orderBy: {
+			updatedAt: "desc",
+		},
+		include: {
+			tags: true,
+			images: true,
+		},
+	});
+
+	return post;
+}
+
+export async function getTags() {
+	const tagList = await prisma.postTag.findMany({});
+
+	const uniqueTag = [...new Set(tagList.map((item) => item.tagId))];
+
+	console.log(uniqueTag);
+
+	const tags = await prisma.tag.findMany({});
+
+	const filteredTag = tags.filter((item) => uniqueTag.includes(item.id));
+
+	console.log(filteredTag);
+
+	return filteredTag;
+}
+
+export async function getPostByTag(tagName: string) {
+	const filteredPosts = await prisma.post.findMany({
+		where: {
+			tags: {
+				some: {
+					tag: {
+						name: tagName,
+					},
+				},
+			},
+		},
+		orderBy: {
+			id: "desc",
+		},
+		include: {
+			tags: true,
+			images: true,
+		},
+	});
+
+	return filteredPosts;
 }
