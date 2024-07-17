ba0b97ca68f2a32e22ae5014cc8cf91d2973bbd0
GYU HAN <ruehan98@gmail.com>
2024-07-17 22:35:17 +0900

    Feat : 설정 파일 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	images: {
+		remotePatterns: [
+			{
+				hostname: "imagedelivery.net",
+			},
+		],
+	},
+};
 
 export default nextConfig;
